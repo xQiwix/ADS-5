77e49d032cdd694d676a32635ed5f81be8a0243a
xQiwix <159818062+xQiwix@users.noreply.github.com>
2024-05-03 17:13:19 +0300

    Update tstack.hB176

--- include/tstack.h
+++ include/tstack.h
@@ -2,9 +2,45 @@
 #ifndef INCLUDE_TSTACK_H_
 #define INCLUDE_TSTACK_H_
 
-template<typename T, int size>
+template<typename T, int SIZE>
 class TStack {
-  // добавьте код стека
+private:
+  T *data = new T[100];
+  int top = 0, size = 100;
+
+ public:
+  TStack() {
+    data = new T[SIZE];
+    size = SIZE;
+    top = 0;
+  }
+  ~TStack() { delete[] data; }
+  void push(T item) {
+    data[top] = item;
+    top++;
+  }
+  T pop() {
+    if (top == 0) {
+      throw "Stack is empty!";
+    } else {
+      top -= 1;
+      return data[top];
+    }
+  }
+  T check() {
+    if (top == 0) {
+      throw "Stack is empty!";
+    } else {
+      return data[top - 1];
+    }
+  }
+  bool isempty() {
+    if (top == 0) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 };
 
 #endif  // INCLUDE_TSTACK_H_
